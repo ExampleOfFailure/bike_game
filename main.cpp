575fcd0ec3fdb4d7bdfb8cfafb53d459793956bb
ExampleOfFailure <alishbarafique5@gmail.com>
2026-04-25 12:39:50 +0500

    wow

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
-using namespace std; 
+#include <SFML/Graphics.hpp>
 
-int main(){
-    cout<<"wow";
+int main() {
+    // Change the window title to something cute!
+auto window = sf::RenderWindow(sf::VideoMode({800, 600}), "Bike Adventure");
+
+// ... inside the while loop ...
+
+// Use a custom RGB color for a soft Pink/Lavender touch
+// (Red, Green, Blue)
+window.clear(sf::Color(255, 182, 193)); // Light Pink.
+    return 0;
 }
